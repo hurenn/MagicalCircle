92e1ec5ea6288b0dc386fc1003e8d3ed117b4df0
hurenn <suzukihuren070@gmail.com>
2024-03-13 02:58:32 +0900

    サンダー実装

---
+++ Source/MagicCircle/Private/Magics/MagicThander.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Magics/MagicThander.h"
+
+void MagicThander::Execute() {
+
+}
---
+++ Source/MagicCircle/Public/Magics/MagicThander.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Magics/MagicBase.h"
+#include "MagicThander.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICCIRCLE_API UMagicThander : public UMagicBase
+{
+	GENERATED_BODY()
+public:
+	virtual void Execute()override;
+};
