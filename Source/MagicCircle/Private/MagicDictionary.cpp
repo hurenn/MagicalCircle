a5e2fddd91102fb297f39bc4335acb405c3effb9
hurenn <suzukihuren070@gmail.com>
2024-05-04 16:56:32 +0900

    チュートリアル序盤作成

--- Source/MagicCircle/Private/MagicDictionary.cpp
+++ Source/MagicCircle/Private/MagicDictionary.cpp
@@ -33,10 +33,11 @@ void UMagicDictionary::Initialize() {
 		{EMagicName::Annihilation, {{EAngle::Up},{EAngle::RightDown},{EAngle::LeftDown},{EAngle::Up},{EAngle::Down},{EAngle::RightUp},{EAngle::LeftUp}, {EAngle::Down}}},
 
 		// イナズマ
-		{EMagicName::Thunder, {{EAngle::Up},{EAngle::Left},{EAngle::Right}, {EAngle::Down}}},
-
-		// 逆イナズマ
+		{EMagicName::ThunderTornado, {{EAngle::Up},{EAngle::Left},{EAngle::Right}, {EAngle::Down}}},
 		{EMagicName::ThunderTornado, {{EAngle::Down},{EAngle::Right},{EAngle::Left}, {EAngle::Up}}},
+
+		// 落雷
+		{EMagicName::Thunder, {{EAngle::Up},{EAngle::Left},{EAngle::Right}, {EAngle::Down}, {EAngle::Up}}},
 
 		// 波動拳
 		{EMagicName::WaterShot, {{EAngle::Down}, {EAngle::RightDown}, {EAngle::Right}}},
